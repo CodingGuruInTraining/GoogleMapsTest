1b6eb111f0777205bd52e03ba006012a4057a5af
DarthVader <edwin.melendez@students.ipr.edu>
2017-03-01 20:09:19 -0600

    fully functional

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,13 +1,21 @@
 import com.google.maps.ElevationApi;
 import com.google.maps.GeoApiContext;
+import com.google.maps.GeocodingApiRequest;
 import com.google.maps.model.ElevationResult;
+import com.google.maps.model.GeocodingResult;
 import com.google.maps.model.LatLng;
 import com.google.maps.GeocodingApi;
-
+import java.util.Scanner;
 import java.io.BufferedReader;
 import java.io.FileReader;
 
+
+
+
 public class Main {
+
+    public static Scanner stringScanner = new Scanner(System.in);
+    public static Scanner numberScanner = new Scanner(System.in);
 
     public static void main(String[] args) throws Exception{
 
@@ -22,17 +30,35 @@ public class Main {
         }
 
 
-        GeoApiContext context = new GeoApiContext().setApiKey(key);
-
-        LatLng mctcLatLng = new LatLng(44.973074, -93.283356);
-
-        ElevationResult[] results = ElevationApi.getByPoints(context, mctcLatLng).await();
+        System.out.println("Where are you?");
+        String userLocation = stringScanner.nextLine();
 
 
-        if (results.length >= 1){
-            ElevationResult mctcElevation = results[0];
-            System.out.println("The elevation of MCTC above sea level is " + mctcElevation.elevation + " meters.");
-            System.out.println(String.format("The elevation of MCTC above sea level is %.2f meters.", mctcElevation.elevation));
+        GeoApiContext context1 = new GeoApiContext().setApiKey(key);
+        GeocodingResult[] geoResult = GeocodingApi.geocode(context1, userLocation).await();
+
+        if (geoResult.length >= 1){
+            for (int i = 0; i <geoResult.length; i++){
+                System.out.println("The result of what you entered " + geoResult[i].formattedAddress);
+            }
+
+            System.out.println("Choose a location result number");
+            int userNumber = numberScanner.nextInt();
+
+
+            LatLng userPick = geoResult[userNumber].geometry.location;
+
+            ElevationResult[] userResults = ElevationApi.getByPoints(context1, userPick).await();
+
+            if(userResults.length >= 1){
+
+                ElevationResult userElevation = userResults[0];
+                System.out.println("Elevation of " + geoResult[0].formattedAddress + " above sea level is " + userElevation.elevation + " meters.");
+                System.out.println(String.format("The elevation of " + geoResult[0].formattedAddress + " above sea level is %.2f meters.", userElevation.elevation));
+            }
+
+
+
         }
 
 
